9ff971c283b1d922fa1ff32d678b295a85adf370
Sun Sreng <sun_sreng@hotmail.com>
2026-04-09 05:29:53 +0700

    chore: remove flutter_widgets example project and update workspace dependencies

--- examples/flutter_widgets/README.md
+++
@@ -1,3 +0,0 @@
-# flutter_widgets
-
-A new Flutter project.
--- examples/flutter_widgets/android/app/src/main/kotlin/co/gmana/flutter_widgets/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package co.gmana.flutter_widgets
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
