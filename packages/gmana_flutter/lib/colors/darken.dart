97429a0abfe9ff1442ee2cc20c1fbd7fe63d0a84
Sun Sreng <sun_sreng@hotmail.com>
2026-05-18 11:03:01 +0700

    refactor: remove unused lighten function from color utilities

--- packages/gmana_flutter/lib/colors/darken.dart
+++ packages/gmana_flutter/lib/colors/darken.dart
@@ -8,12 +8,3 @@ Color darken(Color color, [double amount = .1]) {
 
   return hslDark.toColor();
 }
-
-Color lighten(Color color, [double amount = .1]) {
-  assert(amount >= 0 && amount <= 1);
-
-  final hsl = HSLColor.fromColor(color);
-  final hslLight = hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
-
-  return hslLight.toColor();
-}
